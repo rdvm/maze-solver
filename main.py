fe25ffba237b7c06fe57ffe551bc51489d0c544b
rdvm <git@rvm.dev>
2023-10-29 10:01:32 -0500

    Second phase

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 
 def main():
     win = Window(1200, 900)
+    l = Line(Point(100, 200), Point(200, 80))
+    l2 = Line(Point(600, 700), Point(800, 5))
+    win.draw_line(l, "red")
+    win.draw_line(l2, "red")
     win.wait_for_close()
 
 
