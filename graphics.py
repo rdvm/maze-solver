e50468fd6227994629871652e1ad6312c938f1eb
rdvm <git@rvm.dev>
2023-10-29 19:10:50 -0500

    Final

--- graphics.py
+++ graphics.py
@@ -4,11 +4,11 @@ from tkinter import Tk, BOTH, Canvas
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
-        self.__root.title("Muh window")
-        self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
+        self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -18,13 +18,13 @@ class Window:
         self.__running = True
         while self.__running:
             self.redraw()
-        print("window closed")
+        print("window closed...")
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
 
     def close(self):
         self.__running = False
-
-    def draw_line(self, line, fill_color):
-        line.draw(self.__canvas, fill_color)
 
 
 class Point:
@@ -34,10 +34,17 @@ class Point:
 
 
 class Line:
-    def __init__(self, p1, p2):
+    def __init__(
+        self,
+        p1,
+        p2,
+    ):
         self.p1 = p1
         self.p2 = p2
 
     def draw(self, canvas, fill_color="black"):
-        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
         canvas.pack(fill=BOTH, expand=1)
+
